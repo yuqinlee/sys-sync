79b09480351591c4de634bc8b437974bf95992c3
uchin <yuqin.li@foxmail.com>
2025-10-06 20:13:23 +0800

    add nvim dashbord config

---
+++ config-sync/.config/nvim/lua/plugin/ui/dashboard-nvim.lua
@@ -0,0 +1,20 @@
+return {
+	"nvimdev/dashboard-nvim",
+	dependencies = { { "nvim-tree/nvim-web-devicons" } },
+	event = "VimEnter",
+	config = function()
+		local logo = [[
+            yuchin's neovim  
+        ]]
+
+		logo = string.rep("\n", 8) .. logo .. "\n\n"
+
+		local opts = {
+			theme = "doom",
+			config = {
+				header = vim.split(logo, "\n"),
+			},
+		}
+		require("dashboard").setup(opts)
+	end,
+}
