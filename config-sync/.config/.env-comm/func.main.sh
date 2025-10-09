068237702d5eda13a4af22dd40cec80b87772c8b
uchin <yuqin.li@foxmail.com>
2025-10-09 21:48:05 +0800

    add zsh function for proxy

--- config-sync/.config/.env-comm/func.main.sh
+++ config-sync/.config/.env-comm/func.main.sh
@@ -13,4 +13,29 @@ function f() {
     rm -f -- "$tmp"
 }
 
+# proxy config based v2ray
+proxy_on() {
+    export http_proxy="http://127.0.0.1:20171"
+    export https_proxy="http://127.0.0.1:20171"
+    export ALL_PROXY="socks5://127.0.0.1:20170"
+}
+
+proxy_off() {
+    unset http_proxy https_proxy ALL_PROXY
+}
+# 在右侧显示代理状态
+PROXY_ICON_ON=""     # nf-fa-globe
+PROXY_ICON_OFF=""    # nf-md-lan_disconnect
+proxy_status() {
+  if [[ -n "$http_proxy" || -n "$HTTP_PROXY" ]]; then
+    echo "%F{green}$PROXY_ICON_ON%f"
+  else
+    echo "%F{red}$PROXY_ICON_OFF%f"
+  fi
+}
+
+precmd() {
+  RPROMPT="$(proxy_status)"
+}
+
 [ -f "./func.opt.sh" ] && . "./func.opt.sh"
