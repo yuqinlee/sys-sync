61857cc806c9b5bdee8a0ba0071af68e6a225149
uchin <yuqin.li@foxmail.com>
2025-10-10 11:22:25 +0800

    update input modechange for nvim
    autocmd.lua

--- config-sync/.config/nvim/lua/core/autocmd.lua
+++ config-sync/.config/nvim/lua/core/autocmd.lua
@@ -12,36 +12,33 @@ vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
 			local time = os.date("%I:%M %p")
 
 			-- print nice colored msg
-			vim.api.nvim_echo({ { "󰄳", "LazyProgressDone" }, { " file autosaved at " .. time } }, false, {})
+			vim.api.nvim_echo({ { " 󰄳 ", "LazyProgressDone" }, { " file autosaved at " .. time } }, false, {})
 
 			clear_cmdarea()
 		end
 	end,
 })
 
--- 输入法切换
--- 仅在 Linux 且安装了 fcitx5-remote 时启用
+-- fcitx5 输入法切换
 if vim.fn.has("unix") == 1 and vim.fn.executable("fcitx5-remote") == 1 then
-	local fcitx5_last_status = 1
+	local fcitx5_last_status = tonumber("1")
 
-	-- 进入 Normal 模式时，强制切到英文，并保存之前的状态
+	-- insert 模式进入任何模式都会切换英文并记录原状态
 	vim.api.nvim_create_autocmd("ModeChanged", {
-		pattern = "*:n", -- 进入 normal
+		pattern = "i:*", -- 进入 normal 模式
 		callback = function()
 			fcitx5_last_status = tonumber(vim.fn.system("fcitx5-remote"))
-			vim.fn.system("fcitx5-remote -c") -- 强制英文
+			vim.fn.system("fcitx5-remote -c") -- 切英文
 		end,
 	})
 
-	-- 离开 Normal 模式时（进入 insert/visual/cmdline 等），恢复原状态
+	-- 任何模式进入 insert 模式后都会恢复原先 insert 模式状态
 	vim.api.nvim_create_autocmd("ModeChanged", {
-		pattern = "n:*", -- 从 normal 切到其他模式
+		pattern = "*:i", -- 仅限从 normal 到 insert
 		callback = function()
 			if fcitx5_last_status == 2 then
 				vim.fn.system("fcitx5-remote -o") -- 恢复中文
 			end
 		end,
 	})
-else
-	vim.notify("fcitx5 integration disabled: not Linux or fcitx5-remote not found", vim.log.levels.INFO)
 end
