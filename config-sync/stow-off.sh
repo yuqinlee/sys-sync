36992db772d3747fc2197a70fbe224d2d97e201e
yuchin <yuqin.li@foxmail.com>
2026-02-09 16:18:25 +0800

    tmp

--- config-sync/stow-off.sh
+++ config-sync/stow-off.sh
@@ -1,5 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
+# cancel public config stow
 stow \
+    --target ~ . \
     -D \
-    -t ~ .
+    --ignore multi \
+    --ignore etc \
+    --ignore stow-on.sh \
+    --ignore stow-off.sh
+
+# cancel multi device config stow
+HOST="$(uname -n)"
+STOW_DIR="./multi"
+
+if [[ -d "$STOW_DIR/$HOST" ]]; then
+    stow --dir "$STOW_DIR" --target "$HOME" -D "$HOST"
+fi
--- config-sync/stow-on.sh
+++ config-sync/stow-on.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+set -euo pipefail
 
+# public config stow
 stow \
     --target ~ . \
     --adopt \
+    --ignore multi \
+    --ignore etc \
     --ignore stow-on.sh \
     --ignore stow.off.sh \
-    --ignore etc \
+
+# multi device config stow
+HOST="$(uname -n)"
+STOW_DIR="./multi"
+
+if [[ -d "$STOW_DIR/$HOST" ]]; then
+     stow --dir "$STOW_DIR" --target "$HOME" "$HOST" --adopt
+fi
