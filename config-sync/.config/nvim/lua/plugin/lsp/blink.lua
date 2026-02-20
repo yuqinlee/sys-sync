de703bac3abec31f8580316b40af4cfc5c9a0cb1
yuchin <yuqin.li@foxmail.com>
2026-02-20 23:13:47 +0800

    nvim config

--- config-sync/.config/nvim/lua/plugin/lsp/blink.lua
+++ config-sync/.config/nvim/lua/plugin/lsp/blink.lua
@@ -1,6 +1,7 @@
 -- 补全
 return {
     "saghen/blink.cmp",
+    version = "1.*",
     enabled = not vim.g.vscode, -- 在vscode-neovim禁用
     dependencies = {
         "rafamadriz/friendly-snippets",
@@ -31,7 +32,7 @@ return {
             preset = "none",
             ["<leader>Tab"] = { "show", "show_documentation", "hide_documentation" },
             -- fallback命令将运行下一个非闪烁键盘映射(回车键的默认换行等操作需要)
-            ["<CR>"] = { "accept", "fallback" },                          -- 更改成'select_and_accept'会选择第一项插入
+            ["<CR>"] = { "accept", "fallback" }, -- 更改成'select_and_accept'会选择第一项插入
             ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
             ["<Tab>"] = { "select_next", "snippet_forward", "fallback" }, -- 同时存在补全列表和snippet时，补全列表选择优先级更高
 
@@ -49,11 +50,11 @@ return {
             -- 不预选第一个项目，选中后自动插入该项目文本
             list = { selection = { preselect = false, auto_insert = false } },
             -- 针对菜单的外观配置
-            -- menu = {
-            -- 	min_width = 15,
-            -- 	max_height = 10,
-            -- 	border = "single", -- Defaults to `vim.o.winborder` on nvim 0.11+
-            -- },
+            menu = {
+                -- min_width = 15,
+                -- max_height = 10,
+                border = "single", -- Defaults to `vim.o.winborder` on nvim 0.11+
+            },
         },
         -- 指定文件类型启用/禁用
         enabled = function()
